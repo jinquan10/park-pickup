2af5ee1a0a576c437b9be1b6e327522bb38a7401
Your Name <you@example.com>
2016-11-23 18:35:36 -0800

    init

---
+++ web/src/main/java/org/parkpickup/etl/DbInit.java
@@ -0,0 +1,7 @@
+package org.parkpickup.etl;
+
+/**
+ * Created by JZ on 11/23/2016.
+ */
+public class DbInit {
+}
---
+++ web/src/main/resources/sql/init_tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS park(
+    id UNIQUE(uuid),
+    name string,
+
+);
