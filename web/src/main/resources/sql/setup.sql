fa7ab1bec2b705f0008c55855a914ab22f653ab1
jz <jinquan10@gmail.com>
2016-11-25 21:33:47 -0800

    dsf

--- web/src/main/resources/sql/setup.sql
+++ web/src/main/resources/sql/setup.sql
@@ -2,5 +2,16 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 
 CREATE TABLE IF NOT EXISTS park(
     id bigint PRIMARY KEY,
+    name text,
+    location GEOGRAPHY(POINT,4236)
+);
 
+CREATE TABLE IF NOT EXISTS person(
+    id text PRIMARY KEY, -- the MAC Address
+    name text
+);
+
+CREATE TABLE IF NOT EXISTS rel_person_park(
+    person_id text,
+    park_id bigint
 );
