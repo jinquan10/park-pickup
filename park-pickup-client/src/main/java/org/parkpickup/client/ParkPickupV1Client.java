2979cd9aa490785597a5c64c1841a251f60ca727
jz <jinquan10@gmail.com>
2016-12-24 14:15:19 -0800

    asfd

--- park-pickup-client/src/main/java/org/parkpickup/client/ParkPickupV1Client.java
+++ park-pickup-client/src/main/java/org/parkpickup/client/ParkPickupV1Client.java
@@ -1,11 +1,12 @@
 package org.parkpickup.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.parkpickup.http.Location;
 import org.parkpickup.http.ParkPickupV1;
 import org.parkpickup.http.exception.RequestFailedException;
 
-import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,11 +16,17 @@ public class ParkPickupV1Client implements ParkPickupV1 {
 
     @Override
     public void updateLocation(String deviceId, Location location) throws RequestFailedException {
-        String path = UriBuilder.fromPath(updateLocationPath).build(deviceId).toASCIIString();
+        String path = updateLocationPath.replaceFirst("\\{deviceId\\}", deviceId);
 
         try {
             URL updateLocationUrl = new URL(clientEnv.getProtocol(), clientEnv.getDomain(), clientEnv.getPort(), path);
+
             HttpURLConnection httpUrlConnection = (HttpURLConnection) updateLocationUrl.openConnection();
+            httpUrlConnection.setDoOutput(true);
+            httpUrlConnection.setRequestMethod("PUT");
+            OutputStreamWriter out = new OutputStreamWriter(httpUrlConnection.getOutputStream());
+            out.write(new ObjectMapper().writeValueAsString(location));
+            out.close();
 
             int responseCode = httpUrlConnection.getResponseCode();
             if (responseCode != 200) {
