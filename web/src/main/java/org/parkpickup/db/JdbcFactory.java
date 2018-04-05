a308700a9759915195eb29043219ec2f41a6d82e
jzjz <jinquan10@gmail.com>
2018-04-04 19:08:01 -0700

    test db

--- web/src/main/java/org/parkpickup/db/JdbcFactory.java
+++ web/src/main/java/org/parkpickup/db/JdbcFactory.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
-import static org.parkpickup.db.DataSourceFactory.appDbName;
-
 @Component
 public class JdbcFactory {
     @Inject
@@ -18,12 +16,12 @@ public class JdbcFactory {
     @Bean
     @DependsOn("persistenceInit")
     public JdbcTemplate initJdbcTemplate() {
-        return new JdbcTemplate(dataSourceFactory.getDataSource(appDbName));
+        return new JdbcTemplate(dataSourceFactory.getDataSource());
     }
 
     @Bean
     @DependsOn("persistenceInit")
     public SimpleJdbcInsert initSimpleJdbcInsert() {
-        return new SimpleJdbcInsert(dataSourceFactory.getDataSource(appDbName));
+        return new SimpleJdbcInsert(dataSourceFactory.getDataSource());
     }
 }
